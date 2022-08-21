d47c71dbd8e0eb18004a9d8e7e93b1c8778c82e0
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2022-08-22 02:22:56 +0600

    bonus marking part left, main project done

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
-//selected Five Part
+//common function
 function getInnerText(getId){
     const playerNameField = document.getElementById(getId);
     const playerName = playerNameField.innerText;
+    return playerName;
+}
+function getInnerValue(getId){
+    const playerNameField = document.getElementById(getId);
+    const playerName = parseInt(playerNameField.value);
     return playerName;
 }
 function setListItem(setId,playerName){
@@ -10,7 +15,11 @@ function setListItem(setId,playerName){
     li.innerText = playerName;
     listContainer.appendChild(li);
 }
-
+function setTextFeild(setId,value){
+    const setField = document.getElementById(setId);
+    setField.innerText = value;
+}
+//selected Five Part
 document.getElementById('select-messi-btn').addEventListener('click', function(){
     const getName = getInnerText('player-messi');
     setListItem('list-container',getName);
@@ -34,4 +43,20 @@ document.getElementById('select-haaland-btn').addEventListener('click', function
 document.getElementById('select-dybala-btn').addEventListener('click', function(){
     const getName = getInnerText('player-dybala');
     setListItem('list-container',getName);
+})
+
+//Budget Part
+document.getElementById('calc-player-expense').addEventListener('click', function(){
+    const perPlayerCost = getInnerValue('per-player-cost');
+    const playerExpense = perPlayerCost * 5;
+    setTextFeild('player-expense-field',playerExpense);
+})
+
+document.getElementById('calc-total-expense').addEventListener('click', function(){
+    const managerCost = getInnerValue('manager-cost');
+    const coachCost = getInnerValue('coach-cost');
+    const playerField = document.getElementById('player-expense-field');
+    const playerCost = parseInt(playerField.innerText)
+    const totalCost = managerCost + coachCost + playerCost;
+    setTextFeild('total-cost-field',totalCost);
 })
