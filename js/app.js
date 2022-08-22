3311da0ecaccf1a2f92bcf282f0657c0952be203
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2022-08-22 12:19:00 +0600

    disabled btn done

--- js/app.js
+++ js/app.js
@@ -19,30 +19,39 @@ function setTextFeild(setId,value){
     const setField = document.getElementById(setId);
     setField.innerText = value;
 }
+function disableButton(btnId) {
+    document.getElementById(btnId).disabled = 'true';
+}
 //selected Five Part
 document.getElementById('select-messi-btn').addEventListener('click', function(){
     const getName = getInnerText('player-messi');
     setListItem('list-container',getName);
+    disableButton('select-messi-btn');
 })
 document.getElementById('select-neymar-btn').addEventListener('click', function(){
     const getName = getInnerText('player-neymar');
     setListItem('list-container',getName);
+    disableButton('select-neymar-btn');
 })
 document.getElementById('select-mbappe-btn').addEventListener('click', function(){
     const getName = getInnerText('player-mbappe');
     setListItem('list-container',getName);
+    disableButton('select-mbappe-btn');
 })
 document.getElementById('select-ronaldo-btn').addEventListener('click', function(){
     const getName = getInnerText('player-ronaldo');
     setListItem('list-container',getName);
+    disableButton('select-ronaldo-btn');
 })
 document.getElementById('select-haaland-btn').addEventListener('click', function(){
     const getName = getInnerText('player-haaland');
     setListItem('list-container',getName);
+    disableButton('select-haaland-btn');
 })
 document.getElementById('select-dybala-btn').addEventListener('click', function(){
     const getName = getInnerText('player-dybala');
     setListItem('list-container',getName);
+    disableButton('select-dybala-btn');
 })
 
 //Budget Part
