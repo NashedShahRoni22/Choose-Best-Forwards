b2ac451399c334006435793bfaa38848e043ce78
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2022-08-23 23:33:08 +0600

    silly mistake solved! ASAP

--- js/app.js
+++ js/app.js
@@ -1,100 +1,55 @@
-//common functions
-//get InnerText
-function getInnerText(getId){
-    const playerNameField = document.getElementById(getId);
-    const playerName = playerNameField.innerText;
-    return playerName;
-}
-//get InnerValue
-function getInnerValue(getId){
-    const playerNameField = document.getElementById(getId);
-    const playerName = parseInt(playerNameField.value);
-    return playerName;
-}
-//set text field
-function setTextFeild(setId,value){
-    const setField = document.getElementById(setId);
-    setField.innerText = value;
-}
-//set player name in list
-function setListItem(setId,playerName){
-    const listContainer = document.getElementById(setId);
-    const li = document.createElement('li');
-    li.innerText = playerName;
-    const playerList = document.querySelectorAll('li');
-    if(playerList.length <= 4){
+const playerNameArray = [];
+
+//display players name
+function displayPlayerName(names){
+    const listContainer = document.getElementById('list-container');
+    listContainer.innerText =''; 
+    for (const name of names) {
+        const li = document.createElement('li');
+        li.innerText = name;
         listContainer.appendChild(li);
     }
-    else{
-        alert('You have already selected 5 players!');
+}
+//add to list
+function addToList(button){
+    const playerName = button.parentNode.children[0].innerText;
+    if(playerNameArray.length <= 4){
+        playerNameArray.push(playerName);
+        button.disabled = 'true';
+        button.style.backgroundColor = 'red';
     }
+    else{
+        alert('Already Added 5 Players!')
+    }
+    displayPlayerName(playerNameArray);
 }
-//disbled btn
-function disableButton(btnId) {
-    const btn = document.getElementById(btnId);
-    btn.disabled = 'true';
+//get inner value
+function getInnerValue(inputId)
+{
+    const inputField =  document.getElementById(inputId).value;
+    const inputFieldValue = parseInt(inputField);
+    return inputFieldValue;
 }
-//selected Five Part
-document.getElementById('select-messi-btn').addEventListener('click', function(){
-    const getName = getInnerText('player-messi');
-    setListItem('list-container',getName);
-    disableButton('select-messi-btn');
-})
-document.getElementById('select-neymar-btn').addEventListener('click', function(){
-    const getName = getInnerText('player-neymar');
-    setListItem('list-container',getName);
-    disableButton('select-neymar-btn');
-})
-document.getElementById('select-mbappe-btn').addEventListener('click', function(){
-    const getName = getInnerText('player-mbappe');
-    setListItem('list-container',getName);
-    disableButton('select-mbappe-btn');
-})
-document.getElementById('select-ronaldo-btn').addEventListener('click', function(){
-    const getName = getInnerText('player-ronaldo');
-    setListItem('list-container',getName);
-    disableButton('select-ronaldo-btn');
-})
-document.getElementById('select-haaland-btn').addEventListener('click', function(){
-    const getName = getInnerText('player-haaland');
-    setListItem('list-container',getName);
-    disableButton('select-haaland-btn');
-})
-document.getElementById('select-dybala-btn').addEventListener('click', function(){
-    const getName = getInnerText('player-dybala');
-    setListItem('list-container',getName);
-    disableButton('select-dybala-btn');
-})
-document.getElementById('select-maria-btn').addEventListener('click', function(){
-    const getName = getInnerText('player-maria');
-    setListItem('list-container',getName);
-    disableButton('select-maria-btn');
-})
-document.getElementById('select-salah-btn').addEventListener('click', function(){
-    const getName = getInnerText('player-salah');
-    setListItem('list-container',getName);
-    disableButton('select-salah-btn');
-})
-document.getElementById('select-xavi-btn').addEventListener('click', function(){
-    const getName = getInnerText('player-xavi');
-    setListItem('list-container',getName);
-    disableButton('select-xavi-btn');
-})
-
-//Budget Part
-document.getElementById('calc-player-expense').addEventListener('click', function(){    
+//get inner text
+function getInnerText(inputId)
+{
+    const inputText =  document.getElementById(inputId).value;
+    const inputTextValue = parseInt(inputText);
+    return inputTextValue;
+}
+//player expense calculation
+function calcPlayerExpense(){
     const perPlayerCost = getInnerValue('per-player-cost');
-    const playerNumber = document.querySelectorAll('li');
-    const playerExpense = perPlayerCost * playerNumber.length;
-    setTextFeild('player-expense-field',playerExpense);
-})
-
-//total expense
-document.getElementById('calc-total-expense').addEventListener('click', function(){
+    const totalPlayer = playerNameArray.length;
+    const playerExpense = perPlayerCost * totalPlayer;
+    document.getElementById('player-expense-field').innerText = playerExpense;
+    return playerExpense;
+}
+//total budget calculation
+function calcTotal(){
+    const playerCost = calcPlayerExpense();
     const managerCost = getInnerValue('manager-cost');
     const coachCost = getInnerValue('coach-cost');
-    const playerField = document.getElementById('player-expense-field');
-    const playerCost = parseInt(playerField.innerText)
-    const totalCost = managerCost + coachCost + playerCost;
-    setTextFeild('total-cost-field',totalCost);
-})
+    const totalCost = playerCost + managerCost + coachCost;
+    document.getElementById('total-cost-field').innerText = totalCost;
+}
